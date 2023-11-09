fbbd6940575a51730552d69406494d3a2afc6870
Adin Izulhaq <adinizulhaq86@gmail.com>
2023-11-09 21:31:41 +0700

    Update timeline section

--- main.js
+++ main.js
@@ -11,6 +11,21 @@ offcanvas.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
 
+// SimplyCountdown
+simplyCountdown(".simply-countdown", {
+  year: 2023, // required
+  month: 12, // required
+  day: 24, // required
+  hours: 8, // Default is 0 [0-23] integer
+  words: {
+    //words displayed into the countdown
+    days: { singular: "hari", plural: "hari" },
+    hours: { singular: "jam", plural: "jam" },
+    minutes: { singular: "menit", plural: "menit" },
+    seconds: { singular: "detik", plural: "detik" },
+  },
+});
+
 // Disable Scroll
 const rootElement = document.querySelector(":root");
 const audioIconWrapper = document.querySelector(".audio-icon-wrapper");
